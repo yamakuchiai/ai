08ac103568b3a7194c31981071dec5188f710bec
yamaguchiai <yamaguchiai@192.168.0.29>
2020-05-21 00:32:08 +0900

    コミットしました

---
+++ gitHub_try/src/gitHub_try/Ai_gitHub_try.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package gitHub_try;
+
+/**
+ * @author yamaguchiai
+ *
+ */
+public class Ai_gitHub_try {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Hello World!");
+		System.out.println("私の好きな食べ物はたらこパスタです。");
+		System.out.println("理由はおいしいからです。");
+	}
+
+}
