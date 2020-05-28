73a279303b595779eaaff8a258debb1513b546df
yamaguchiai <yamaguchiai@192.168.0.29>
2020-05-28 16:50:23 +0900

    commit5

---
+++ git/src/git/git_re_retry.java
@@ -0,0 +1,17 @@
+package git;
+
+public class git_re_retry {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+				System.out.println("私はタラコパスタが好きです。");
+				System.out.println("理由は美味しいから！！！");
+				//System.out.println("ちなみに二番目に好きな食べ物はオクラです");
+				//System.out.println("理由は中学生の時仲が良かった友達の好きな食べ物だからです!!!");
+
+		
+
+	}
+
+}
