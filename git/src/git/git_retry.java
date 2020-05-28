f84e79e8972f92d0ad339a43b6f999b179eac644
yamaguchiai <yamaguchiai@192.168.0.29>
2020-05-28 13:15:29 +0900

    commit4

--- git/src/git/git_retry.java
+++ git/src/git/git_retry.java
@@ -7,7 +7,7 @@ public class git_retry {
 		System.out.println("私はタラコパスタが好きです。");
 		System.out.println("理由は美味しいから！！！");
 		System.out.println("ちなみに二番目に好きな食べ物はオクラです");
-		System.out.println("理由は中学生の時仲が良かった友達の好きな食べ物だからです");
+		System.out.println("理由は中学生の時仲が良かった友達の好きな食べ物だからです!!!");
 	}
 
 }
